78a07c58f84189eeeb75423770bee2cb007b42bd
jeong315 <jeong315@gmail.com>
2020-09-10 23:44:52 +0900

    P107 뷰 기초 속성 활용

--- part2_3/src/main/java/com/example/part2_3/Lab3_3Actibity.java
+++ part2_3/src/main/java/com/example/part2_3/Lab3_3Actibity.java
@@ -3,12 +3,37 @@ package com.example.part2_3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class Lab3_3Actibity extends AppCompatActivity {
+public class Lab3_3Actibity extends AppCompatActivity implements View.OnClickListener {
+
+    Button trueBtn;
+    TextView targetTextView;
+    Button falseBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lab3_3_actibity);
+        //View 객체획득
+
+        trueBtn = findViewById(R.id.btn_visible_true);
+        targetTextView = findViewById(R.id.text_visible_target);
+        falseBtn = findViewById(R.id.btn_visible_false);
+
+        //Button 이벤트 등록
+        trueBtn.setOnClickListener(this);
+        falseBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v){
+        if(v == trueBtn){
+            targetTextView.setVisibility(View.VISIBLE);
+        }else if(v == falseBtn){
+            targetTextView.setVisibility(View.INVISIBLE);
+        }
     }
 }
